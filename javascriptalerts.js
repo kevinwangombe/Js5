5c77b6d3d876d53a07511d4690645fb06d610643
Kevin K Wang'ombe <Kevinwangombe192@gmail.com>
2025-08-13 13:23:38 +0300

    Add files via upload

---
+++ javascriptalerts.js
@@ -0,0 +1,7 @@
+alert('Hello there');
+
+document.write('<p>This is an Alert in JavaScript using single quote.</p>');
+
+alert('Hello there');
+
+document.write('<p>This is an Alert in JavaScript using double qutes.</p>');
